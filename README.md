ee6061739117d9af5ef1af3de53ed363f9a9141e
Fota Maria-Madalina <mfota@deloittece.com>
2023-03-07 11:19:36 +0200

    Weather project commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# The weather component can be found on the details page of the contact records
-# All messages displayed to the client should be added in custom labels, but I did not do this for this project
+1.The weather component can be found on the details page of the contact records
+2.All messages displayed to the client should be added in custom labels, but I did not do this for this project
--- force-app/main/default/lwc/genericError/genericError.js
+++ force-app/main/default/lwc/genericError/genericError.js
@@ -2,8 +2,8 @@ import { LightningElement, api } from 'lwc';
 
 /**************************************************************************************************************
 * @author		Fota Maria-Madalina
-* @date			2023-03-03=4
-* @description  This class contains all client side logic for the BrtxGenericError Lightning Web Component
+* @date			06-03-2023
+* @description  This class contains all client side logic for the genericError Lightning Web Component
 **************************************************************************************************************/
 export default class BrtxGenericError extends LightningElement {
     @api errorMessage;
